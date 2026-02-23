3f2083cd788c83335f7ff29983bfc16f696b0d6d
lhj1618GitHub <125777858+lhj1618GitHub@users.noreply.github.com>
2026-02-23 14:40:45 +0800

    Update com.shuoming.ts

--- src/apps/com.shuoming.ts
+++ src/apps/com.shuoming.ts
@@ -10,25 +10,23 @@ desc:'跳过/跳过广告/关闭',
 actionMaximum: 1,
 matchTime: 4000,
 resetMatch: 'app',
-excludeMatches:'()',
 anyMatches:['[text*="跳过" || text*="跳过广告"|| text*="关闭"][text.length<10][visibleToUser=true]'],
 snapshotUrls:[],
 },
 {
   key: 1,
   name: '弹窗广告',
-  desc: '点击[文字]',
+  desc: '点击[我知道了]',
   fastQuery: true,
   actionMaximum: 1,
-  matchTime: 10000,
   resetMatch: 'app',
   rules: [
     {
       key: 1,
       fastQuery: true,
-      activityIds: '应用及界面id',
-      matches: '[属性值]',
-      snapshotUrls: '快照地址',
+      activityIds: 'com.lhj1618.shuoming.jiemianid',
+      matches: '[desc="我知道了"][visibleToUser=true]',
+      snapshotUrls: '',
          ]
 }
   ]
