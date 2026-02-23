361766a9287e2c42fcbf4af9f2bb0a82a88b3567
lhj1618GitHub <125777858+lhj1618GitHub@users.noreply.github.com>
2026-02-23 17:07:31 +0800

    小米音乐
    v

--- src/apps/com.miui.player.ts
+++ src/apps/com.miui.player.ts
@@ -5,15 +5,44 @@ export default defineGkdApp({
   name: '小米音乐',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          action: 'clickCenter',
+          matches:
+            '[text*="跳过"][text.length<10][width<500 && height<300][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/25094542',
+        },
+      ],
+    },
+    {
       key: 1,
-      name: '弹窗广告-浮窗广告',
-      desc: '关闭右侧飘窗广告',
-      fastQuery: true,
-      activityIds:
-        'com.tencent.qqmusiclite.activity.player.MusicPlayerActivity',
-      rules:
-        '[id="com.miui.player:id/free_mode_tips_layout"] + [id="com.miui.player:id/iv_close"]',
-      snapshotUrls: 'https://i.gkd.li/i/13303283',
+      name: '弹窗广告-悬浮广告',
+      desc: '点击关闭',
+      rules: [
+        {
+          key: 0,
+          fastQuery: true,
+          activityIds:
+            'com.tencent.qqmusiclite.activity.player.MusicPlayerActivity',
+          matches:
+            '[id="com.miui.player:id/free_mode_tips_layout"] + [id="com.miui.player:id/iv_close"]',
+          snapshotUrls: 'https://i.gkd.li/i/13303283',
+        },
+        {
+          key: 1,
+          fastQuery: true,
+          activityIds: 'com.tencent.qqmusiclite.activity.MainActivity',
+          matches:
+            '[id="com.miui.player:id/cl_floating_promote"] > [id="com.miui.player:id/iv_close"]',
+          snapshotUrls: 'https://i.gkd.li/i/13562649',
+        },
+      ],
     },
     {
       key: 2,
@@ -35,7 +64,7 @@ export default defineGkdApp({
         },
         {
           key: 1,
-          matches: '[text="广告｜跳过"]', // 不使用vid="ad_skip_text"，避免和 key 0 冲突
+          matches: '[text="广告｜跳过"]', // 不使用vid="ad_skip_text"，避免和key 0冲突
           exampleUrls:
             'https://m.gkd.li/57941037/2ae1ca24-fb50-48ff-9343-e5607b512127',
           snapshotUrls: 'https://i.gkd.li/i/14711960',
@@ -44,48 +73,49 @@ export default defineGkdApp({
     },
     {
       key: 3,
-      name: '弹窗广告-看广告免费听弹窗',
-      activityIds: 'com.tencent.qqmusiccommon.hybrid.HybridViewActivity',
-      rules: 'ViewGroup > @ViewGroup + ViewGroup[childCount=5]',
-      snapshotUrls: ['https://i.gkd.li/i/13304343'],
+      name: '弹窗广告',
+      desc: '点击关闭',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          fastQuery: true,
+          activityIds: 'com.tencent.qqmusiccommon.hybrid.HybridViewActivity',
+          matches:
+            '@ViewGroup[clickable=true][childCount=0][visibleToUser=true] < ViewGroup[childCount=1] < ViewGroup[childCount=2] < FrameLayout < FrameLayout < [vid="webview_container"]',
+          exampleUrls: 'https://e.gkd.li/5e6fd5f8-6ad3-4a46-ab34-c9b6b2d7a112',
+          snapshotUrls: 'https://i.gkd.li/i/17783777',
+        },
+        {
+          key: 1,
+          activityIds: 'com.tencent.qqmusiccommon.hybrid.HybridViewActivity',
+          matches: '[id=null][desc~=".*关闭.*按钮.*"][clickable=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/12700955',
+            'https://i.gkd.li/i/22562104',
+          ],
+        },
+      ],
     },
     {
       key: 4,
-      name: '弹窗广告-首页右侧悬浮卡片',
-      activityIds: 'com.tencent.qqmusiclite.activity.MainActivity',
-      fastQuery: true,
-      rules:
-        '[id="com.miui.player:id/cl_floating_promote"] > [id="com.miui.player:id/iv_close"]',
-      snapshotUrls: ['https://i.gkd.li/i/13562649'],
+      name: '弹窗广告-部分误触',
+      desc: '注意：会关闭年度报告邀请函弹窗',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.tencent.qqmusiclite.activity.MainActivity',
+          matches: '[id="com.miui.player:id/iv_close_dialog_button"]',
+          snapshotUrls: 'https://i.gkd.li/i/13623503',
+        },
+      ],
     },
     {
       key: 5,
-      name: '自动化-年度报告邀请函弹窗',
-      desc: '点击关闭',
-      activityIds: 'com.tencent.qqmusiclite.activity.MainActivity',
-      fastQuery: true,
-      rules: '[id="com.miui.player:id/iv_close_dialog_button"]',
-      snapshotUrls: ['https://i.gkd.li/i/13623503'],
-    },
-    {
-      key: 6,
-      name: '弹窗广告-会员过期续费弹窗',
-      activityIds: 'com.tencent.qqmusiccommon.hybrid.HybridViewActivity',
-      rules: '[id=null][desc="关闭弹框按钮"][clickable=true]',
-      snapshotUrls: 'https://i.gkd.li/i/12700955',
-    },
-    {
-      key: 7,
-      name: '内嵌广告-"我的"页面顶部卡片广告',
-      activityIds: 'com.tencent.qqmusiclite.activity.MainActivity',
-      rules:
-        '[id="com.miui.player:id/banner_image"] + [id="com.miui.player:id/close_banner"]',
-      snapshotUrls: 'https://i.gkd.li/i/12700984',
-    },
-    {
-      key: 8,
       name: '弹窗广告-看广告视频领VIP',
-      desc: '等待 15s 自动点击"退出"',
+      desc: '等待15s自动点击"退出"',
       rules: [
         {
           actionDelay: 15000,
@@ -93,6 +123,51 @@ export default defineGkdApp({
           activityIds: 'com.tencentmusic.ad.tmead.reward.TMERewardActivity',
           matches: '[id="com.miui.player:id/tme_ad_skip_button"]',
           snapshotUrls: 'https://i.gkd.li/i/13610667',
+        },
+      ],
+    },
+    {
+      key: 6,
+      name: '内嵌广告-主界面卡片广告',
+      rules: [
+        {
+          key: 1,
+          fastQuery: true,
+          activityIds: 'com.tencent.qqmusiclite.activity.MainActivity',
+          matches: '[vid="ad_close"][text=null]',
+          exampleUrls: 'https://e.gkd.li/32ad4d0f-8992-45b0-9e1f-82ce2cc58dcb',
+          snapshotUrls: 'https://i.gkd.li/i/16773614',
+        },
+        {
+          key: 2,
+          fastQuery: true,
+          activityIds: 'com.tencent.qqmusiclite.activity.MainActivity',
+          matches: '[vid="banner_divider"] - * > [vid="close_banner"]',
+          exampleUrls: 'https://e.gkd.li/d9b74767-84b6-4668-8a9b-261bd938a8a3',
+          snapshotUrls: 'https://i.gkd.li/i/20420253',
+        },
+      ],
+    },
+    {
+      key: 7,
+      name: '内嵌广告-首页卡片广告',
+      rules: [
+        {
+          key: 0,
+          fastQuery: true,
+          activityIds: 'com.tencent.qqmusiclite.activity.MainActivity',
+          matches: '[vid="ad_more"]',
+          exampleUrls: 'https://e.gkd.li/e7d3d6c3-d8be-4e0f-ac02-e5df2a0615b3',
+          snapshotUrls: 'https://i.gkd.li/i/20420265',
+        },
+        {
+          preKeys: [0],
+          key: 20,
+          fastQuery: true,
+          activityIds: 'com.tencent.qqmusiclite.activity.MainActivity',
+          matches: '[vid="ad_close"][text="不感兴趣"]',
+          exampleUrls: 'https://e.gkd.li/6ca7beba-98b8-4892-b275-1a116d040115',
+          snapshotUrls: 'https://i.gkd.li/i/20420322',
         },
       ],
     },
