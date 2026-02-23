dfe9a51468990ff9fd45171a1380775cf964e3dc
lhj1618GitHub <125777858+lhj1618GitHub@users.noreply.github.com>
2026-02-23 13:13:50 +0800

    Update globalGroups.ts

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -1,3 +1,28 @@
 import { defineGkdGlobalGroups } from '@gkd-kit/define';
 
-export default defineGkdGlobalGroups([]);
+export default defineGkdGlobalGroups([
+//activityIds:活动id=应用id+界面id
+  {
+  key: 1,
+  name: '内嵌广告-1',
+  "desc": "功能描述",
+  rules: [
+    {
+      key: 0,
+      fastQuery: true,
+      activityIds: '应用及界面id',
+      matches: '[属性值]',
+      snapshotUrls: '快照地址',
+      [visibleToUser=true]"],
+    },
+    {
+      key: 1,
+      fastQuery: true,
+      activityIds: '应用及界面id',
+      matches: '[属性值]',
+      snapshotUrls: '快照地址',
+      [visibleToUser=true]"],
+    },
+  ],
+}
+]);
