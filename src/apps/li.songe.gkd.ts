e38cafbdb6d77f3261fecc0bbcd6c3b244580e6e
lhj1618GitHub <125777858+lhj1618GitHub@users.noreply.github.com>
2026-02-23 14:21:41 +0800

    Update li.songe.gkd.ts

--- src/apps/li.songe.gkd.ts
+++ src/apps/li.songe.gkd.ts
@@ -3,19 +3,7 @@ import { defineGkdApp } from '@gkd-kit/define';
 export default defineGkdApp({
   id: 'li.lhj1618.gkd',
   name: 'GKD',
-  groups: [
-{key:0,
-name:'开屏广告',
-desc:'跳过/跳过广告/关闭',
-actionMaximum: 1,
-matchTime: 4000,
-resetMatch: 'app',
-excludeMatches:'()',
-anyMatches:['[text*="跳过" || text*="跳过广告"|| text*="关闭"][text.length<10][visibleToUser=true]'],
-snapshotUrls:[],
-excludeSnapshotUrls:[]
-},
-    
+  groups: [   
 {
   key: 1,
   name: '弹窗广告',
@@ -33,5 +21,5 @@ excludeSnapshotUrls:[]
       snapshotUrls: '快照地址',
          ]
 }
-    ]
+        ]
 });
