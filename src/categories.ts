ad770ea3391f4d3b560024dca8823c42f2fd0319
lhj1618GitHub <125777858+lhj1618GitHub@users.noreply.github.com>
2026-02-23 15:35:09 +0800

    Update categories.ts

--- src/categories.ts
+++ src/categories.ts
@@ -1,3 +1,42 @@
 import { defineGkdCategories } from '@gkd-kit/define';
 
 export default defineGkdCategories([]);
+
+
+import { defineGkdCategories } from '@gkd-kit/define';
+
+export default defineGkdCategories([
+  {
+    key: 0,
+    name: '开屏广告',
+    enable: true,
+  },
+  {
+    key: 1,
+    name: '弹窗广告',
+    enable: false,
+  },
+  {
+    key: 2,
+    name: '内嵌广告',
+    enable: false,
+  },
+  {
+    //系统功能自动化
+    key: 3,
+    name: '系统自动化',
+    enable: false,
+  },
+  {
+    // 软件内自动化
+    key: 4,
+    name: '软件内自动化',
+    enable: false,
+  },
+  {
+    // 分类不明确
+    key: 5,
+    name: '其它',
+    enable: false,
+  },
+]);
