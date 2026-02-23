b1a658df1e4aa6cebfbe65325040bdcbc06a10ce
lhj1618GitHub <125777858+lhj1618GitHub@users.noreply.github.com>
2026-02-23 14:05:39 +0800

    Fix syntax error in globalGroups.ts

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -2,7 +2,7 @@ import { defineGkdGlobalGroups } from '@gkd-kit/define';
 
 export default defineGkdGlobalGroups([
 //全局
-  {key:0,
+{key:0,
 name:'开屏广告',
 desc:'跳过/跳过广告/关闭',
 actionMaximum: 1,
@@ -12,5 +12,5 @@ excludeMatches:'()',
 anyMatches:['[text*="跳过" || text*="跳过广告"|| text*="关闭"][text.length<10][visibleToUser=true]'],
 snapshotUrls:[],
 excludeSnapshotUrls:[]
-},
+}
 ]);
