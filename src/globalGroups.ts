9f07d0c659318688197f1ddedddb6e66286f4071
lhj1618GitHub <125777858+lhj1618GitHub@users.noreply.github.com>
2026-02-23 14:34:05 +0800

    Update globalGroups.ts

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -1,16 +1,3 @@
 import { defineGkdGlobalGroups } from '@gkd-kit/define';
 
-export default defineGkdGlobalGroups([ 
-{
-key:0,
-name:'开屏广告',
-desc:'跳过/跳过广告/关闭',
-actionMaximum: 1,
-matchTime: 4000,
-resetMatch: 'app',
-excludeMatches:'()',
-anyMatches:['[text*="跳过" || text*="跳过广告"|| text*="关闭"][text.length<10][visibleToUser=true]'],
-snapshotUrls:[],
-excludeSnapshotUrls:[]
-}
-]);
+export default defineGkdGlobalGroups([]);
