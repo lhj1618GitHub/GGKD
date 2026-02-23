0be233f165647448719cf5728afc013dc04898a0
lhj1618GitHub <125777858+lhj1618GitHub@users.noreply.github.com>
2026-02-23 14:25:11 +0800

    Update li.songe.gkd.ts

--- src/apps/li.songe.gkd.ts
+++ src/apps/li.songe.gkd.ts
@@ -1,25 +1,7 @@
 import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
-  id: 'li.lhj1618.gkd',
+  id: 'li.songe.gkd',
   name: 'GKD',
-  groups: [   
-{
-  key: 1,
-  name: '弹窗广告',
-  desc: '点击[文字]',
-  fastQuery: true,
-  actionMaximum: 1,
-  matchTime: 10000,
-  resetMatch: 'app',
-  rules: [
-    {
-      key: 1,
-      fastQuery: true,
-      activityIds: '应用及界面id',
-      matches: '[属性值]',
-      snapshotUrls: '快照地址',
-         ]
-}
-        ]
+  groups: []
 });
