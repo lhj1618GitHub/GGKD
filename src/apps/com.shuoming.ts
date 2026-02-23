165d9f127bc37f2ff2cd758d37e7d58d1acd0eb4
lhj1618GitHub <125777858+lhj1618GitHub@users.noreply.github.com>
2026-02-23 14:46:11 +0800

    Update com.shuoming.ts

--- src/apps/com.shuoming.ts
+++ src/apps/com.shuoming.ts
@@ -4,22 +4,10 @@ export default defineGkdApp({
   id: 'com.lhj1618.shuoming',
   name: '说明',
   groups: [
-{key:0,
-name:'开屏广告',
-desc:'跳过/跳过广告/关闭',
-actionMaximum: 1,
-matchTime: 4000,
-resetMatch: 'app',
-anyMatches:['[text*="跳过" || text*="跳过广告"|| text*="关闭"][text.length<10][visibleToUser=true]'],
-snapshotUrls:[],
-},
 {
   key: 1,
   name: '弹窗广告',
   desc: '点击[我知道了]',
-  fastQuery: true,
-  actionMaximum: 1,
-  resetMatch: 'app',
   rules: [
     {
       key: 1,
