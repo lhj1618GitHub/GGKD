ab566a3d762d2930745f11724ef7291e09d18247
lhj1618GitHub <125777858+lhj1618GitHub@users.noreply.github.com>
2026-02-23 15:15:22 +0800

    Update com.shuoming.ts

--- src/apps/com.shuoming.ts
+++ src/apps/com.shuoming.ts
@@ -1,21 +1,20 @@
 import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
+{
   id: 'com.lhj1618.shuoming',
   name: '说明',
   groups: [
-{
-  key: 1,
-  name: '弹窗广告',
-  desc: '点击[我知道了]',
-  rules: [
     {
       key: 1,
-      fastQuery: true,
-      activityIds: 'com.lhj1618.shuoming.jiemianid',
-      matches: '[desc="我知道了"][visibleToUser=true]',
-      snapshotUrls: '',
-         ]
+      name: '弹窗广告',
+      rules: { matches: '[desc="我知道了"][visibleToUser=true]' },
+      snapshotUrls: ['https://i.gkd.li/i/****'],
+   },
+         ],
 }
-  ]
 });
+
+
+
+
