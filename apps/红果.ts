de17802ac156087ad26fdd7f16207749cabbff37
lhj1618 <lhj1618@outlook.com>
2026-03-26 06:38:57 +0000

    rename apps/测试.ts to apps/红果.ts.
    Signed-off-by: lhj1618 <lhj1618@outlook.com>

---
+++ apps/红果.ts
@@ -0,0 +1,15 @@
+{
+      key: 1,
+      name: '弹窗广告-关闭宝箱结算',
+      desc: '点击金币数最后一位的相对位置'
+      rules: [
+        {
+          activityIds: 'com.dragon.read.pages.main.MainFragmentActivity',
+          position: {
+            top: 'width * 13.5000',
+            left: 'width * -5.2500',
+          },
+          matches: '@ViewGroup[width=24][height=58][index=7]',
+        },
+      ],
+},
