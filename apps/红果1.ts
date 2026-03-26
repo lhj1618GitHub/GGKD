9f277a8815540cc490f044f6e350153a739ba14d
lhj1618 <lhj1618@outlook.com>
2026-03-26 07:16:25 +0000

    add apps/红果1.ts.
    Signed-off-by: lhj1618 <lhj1618@outlook.com>

---
+++ apps/红果1.ts
@@ -0,0 +1,38 @@
+{
+  key: 1,
+  name: '弹窗广告-宝箱相关',
+  desc: '领取/结算',
+  rules: [
+    {
+      key: 0,
+      name: '开启宝箱',
+      position: {
+        left: 'width * 0.5271',
+        top: 'width * 0.5934',
+      },
+      matches: [
+        'ViewGroup[width=18][height=220][index=1]',
+        '@ViewGroup[width=332][height=222]',
+      ],
+      fastQuery: true,
+      activityIds: [
+        'com.dragon.read.pages.main.MainFragmentActivity',
+      ],
+    },
+    {
+      key: 1,
+      name: '关闭宝箱结算',
+      position: {
+        left: 'width * -5.2500',
+        top: 'width * 13.5000',
+      },
+      matches: [
+        '@ViewGroup[width=24][height=58][index=7]',
+      ],
+      fastQuery: true,
+      activityIds: [
+        'com.dragon.read.pages.main.MainFragmentActivity',
+      ],
+    },
+  ],
+}
