ecaf746af41552f8a4e74cfd597437cb48ff3c0c
lhj1618 <lhj1618@outlook.com>
2026-03-28 15:59:19 +0000

    update  apps/com.dragon.read.ts.
    Signed-off-by: lhj1618 <lhj1618@outlook.com>

--- apps/com.dragon.read.ts
+++ apps/com.dragon.read.ts
@@ -1,9 +1,16 @@
 {
+  id: 'com.phoenix.read',
+  name: '红果免费短剧',
+  groups: [
+    {
   key: 1,
   name: '弹窗广告-宝箱相关',
   desc: '领取/结算',
   fastQuery: true,
   matchTime: 10000,
+  activityIds: [
+    'com.dragon.read.pages.main.MainFragmentActivity',
+  ],
   rules: [
     {
       key: 0,
@@ -15,10 +22,6 @@
       matches: [
         '@ViewGroup[width=18][index=0][visibleToUser=true]',
       ],
-      fastQuery: true,
-      activityIds: [
-        'com.dragon.read.pages.main.MainFragmentActivity',
-      ],
     },
     {
       key: 1,
@@ -28,12 +31,44 @@
         top: 'width * 8',
       },
       matches: [
-        '@ViewGroup[desc="第?个"][width=66][visibleToUser=true]',
+        '@ViewGroup[desc="第?个"][visibleToUser=true]',
+      ],
+    },
+  ],
+},
+{
+      key: 2,
+      name: '内嵌广告-剧集广告',
+      desc: '广告预警时[返回]',
+      rules: [
+        {
+          fastQuery: true,
+          action: 'back',
+          activityIds: 'com.dragon.read.component.shortvideo.impl.ShortSeriesActivity',
+          position: {
+            top: 'width * -6.8161',
+            left: 'width * -1.0806',
+          },
+          matches: '[vid="an1"][text="2s后即将进入广告"]',
+        },
+      ],
+    },
+{
+  key: 3,
+  name: '弹窗广告-推送提醒',
+  desc: '点[取消]',
+  rules: [
+    {
+      matches: [
+        '[vid="j3"][text="开启推送提醒"]',
+        '[vid="cm_"]',
       ],
       fastQuery: true,
       activityIds: [
-        'com.dragon.read.pages.main.MainFragmentActivity',
+        'com.dragon.read.component.shortvideo.impl.ShortSeriesActivity',
       ],
     },
+  ],
+},
   ],
 }
