9c28488b4ac1f891b699ad10b4969e0b3f5c5953
lhj1618 <lhj1618@outlook.com>
2026-03-30 05:58:42 +0000

    update apps/全局规则.ts.
    Signed-off-by: lhj1618 <lhj1618@outlook.com>

--- apps/全局规则.ts
+++ apps/全局规则.ts
@@ -1,10 +1,16 @@
 {
-key:0,
-name:'开屏广告',
-desc:'点击[跳过/关闭]',
-fastQuery: true,
-actionMaximum: 1,
-matchTime: 5000,
-resetMatch: 'app',
-anyMatches:['[text*="跳过" || text*="关闭"][text.length<10][visibleToUser=true]'],
+    key: 0,
+    name: '开屏广告-全局',
+    desc: '关闭打开应用时的开屏广告',
+    fastQuery: true,
+    matchTime: 5000,
+    actionMaximum: 1,
+    resetMatch: 'app',
+    rules: [
+        {
+            anyMatches: [
+                '[text*="跳过" || text="关闭"][text.length<10][visibleToUser=true][clickable=true]',
+            ],
+        },
+    ],
 }
