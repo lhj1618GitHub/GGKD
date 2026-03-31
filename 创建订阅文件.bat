5519c7b9c47b5b2020c78fc155be018ecb3a67c8
lhj1618 <lhj1618@outlook.com>
2026-03-31 10:17:51 +0000

    主
    Signed-off-by: lhj1618 <lhj1618@outlook.com>

---
+++ 创建订阅文件.bat
@@ -0,0 +1,23 @@
+@echo off
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+
+echo ========== 主脚本开始执行 ==========
+echo.
+
+:: 调用todo目录下的批处理文件
+call todo/to-one.bat
+if errorlevel 1 (echo 合并失败 & pause & exit /b 1)
+
+call todo/to-in.bat
+if errorlevel 1 (echo 替换失败 & pause & exit /b 1)
+
+:: 删除根目录下生成的临时文件
+del "all-apps.ts"
+
+echo.
+echo ========== ✅ 全部执行完成 ==========
+echo 最终文件：new.json5
+echo 请手动修改-版本号 +1
+echo.
+pause
