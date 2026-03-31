6bf48d4fbed33ae8a7b15c095ecb9d89eab98202
lhj1618 <lhj1618@outlook.com>
2026-03-31 19:22:35 +0000

    update  apps/GKD.ts.
    Signed-off-by: lhj1618 <lhj1618@outlook.com>

--- apps/GKD.ts
+++ apps/GKD.ts
@@ -6,7 +6,7 @@
             key: 1,
             name: '自动化-跳转链接',
             desc: '[打开]',
-            matchTime: 3000,
+            matchTime: 5000,
             rules: [
                 {
                     key: 1,
