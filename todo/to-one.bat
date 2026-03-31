824f7487d67593c348002c074a67ccc208acf919
lhj1618 <lhj1618@outlook.com>
2026-03-31 10:16:43 +0000

    创建
    Signed-off-by: lhj1618 <lhj1618@outlook.com>

---
+++ todo/to-one.bat
@@ -0,0 +1,34 @@
+@echo off
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+
+:: 自动定位到【项目根目录】，不管从哪里运行都正确
+PUSHD "%~dp0.."
+set "ROOT_DIR=%CD%"
+POPD
+
+set "TS_SOURCE_DIR=%ROOT_DIR%\apps"
+set "TS_OUTPUT=%ROOT_DIR%\all-apps.ts"
+
+if not exist "%TS_SOURCE_DIR%" (
+    echo 错误：找不到文件夹 !TS_SOURCE_DIR!
+    pause
+    exit /b 1
+)
+
+echo 正在清空输出文件...
+echo. > "%TS_OUTPUT%"
+
+set "first=1"
+for %%f in ("%TS_SOURCE_DIR%\*.ts") do (
+    if !first!==1 (
+        type "%%f" >> "%TS_OUTPUT%"
+        set "first=0"
+    ) else (
+        echo , >> "%TS_OUTPUT%"
+        type "%%f" >> "%TS_OUTPUT%"
+    )
+)
+
+echo ✅ TS 合并完成：%TS_OUTPUT%
+echo.
