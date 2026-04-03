4851f9ff571cfe247defdf586006ade9a5d632b4
lhj1618 <lhj1618@outlook.com>
2026-04-03 08:24:40 +0000

    update 创建订阅文件.bat.
    Signed-off-by: lhj1618 <lhj1618@outlook.com>

--- 创建订阅文件.bat
+++ 创建订阅文件.bat
@@ -6,17 +6,19 @@ echo ========== 主脚本开始执行 ==========
 echo.
 
 :: 调用todo目录下的批处理文件
+echo 开始合并所有应用规则
 call todo/to-one.bat
-if errorlevel 1 (echo ❌ 合并失败 & pause & exit /b 1)
+if errorlevel 1 (echo 合并失败 & pause & exit /b 1)
 
+echo 开始整理完整订阅文件
 call todo/to-in.bat
-if errorlevel 1 (echo ❌ 替换失败 & pause & exit /b 1)
+if errorlevel 1 (echo 替换失败 & pause & exit /b 1)
+
 
 :: 删除根目录下生成的临时文件
-del "all-apps.ts"
+echo 开始删除多余文件
 echo.
-echo ✅ all-apps.ts 文件删除已执行！
-echo.
+del -f "all-apps.ts"
 
 echo.
 echo ========== ✅ 全部执行完成 ==========
