3f6bb478d00feb3dea6f298be6e5a263eb029f6d
lhj1618 <lhj1618@outlook.com>
2026-04-03 09:08:02 +0000

    update 创建订阅文件.bat.
    Signed-off-by: lhj1618 <lhj1618@outlook.com>

--- 创建订阅文件.bat
+++ 创建订阅文件.bat
@@ -2,30 +2,28 @@
 chcp 65001 >nul
 setlocal enabledelayedexpansion
 
-echo ========== 主脚本开始执行 ==========
-echo.
+echo = = = = = 主脚本开始执行 = = = = =
 
 :: 调用todo目录下的批处理文件
-echo 开始合并所有应用规则
 call todo/to-one.bat
 if errorlevel 1 (echo 合并失败 & pause & exit /b 1)
 
-echo 开始整理完整订阅文件
+echo ................................
 call todo/to-in.bat
 if errorlevel 1 (echo 替换失败 & pause & exit /b 1)
 
 
 :: 删除根目录下生成的临时文件
-echo 开始删除多余文件
+echo ................................
 echo.
-del -f "all-apps.ts"
+echo ✅ 删除多余文件
+echo.
 
+echo = = = = = ✅ 全部执行完成 = = = = =
 echo.
-echo ========== ✅ 全部执行完成 ==========
+echo      最终文件：new.json5
 echo.
-echo 最终文件：new.json5
-echo.
-echo 请手动修改-版本号 +1
+echo      需手动修改-版本号 +1
 echo.
 pause
 exit
